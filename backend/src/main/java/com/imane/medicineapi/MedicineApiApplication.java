139ac8520d1579e71f5ec53f4b70f191f68da31e
samadi imane <imanesamadi21@gmail.com>
2026-05-07 15:39:55 +0000

    Initial Spring Boot backend setup

---
+++ backend/src/main/java/com/imane/medicineapi/MedicineApiApplication.java
@@ -0,0 +1,13 @@
+package com.imane.medicineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/imane/medicineapi/MedicineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imane.medicineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
